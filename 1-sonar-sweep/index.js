ba33f5419d856df6a4ad72e7353d76ee83f8913b
Amruth Pillai <im.amruth@gmail.com>
2021-12-02 10:10:22 +0100

    - completed 2-dive - add README.md for each day

--- 1-sonar-sweep/index.js
+++ 1-sonar-sweep/index.js
@@ -17,6 +17,7 @@ const main = async () => {
 
   // Loop through input array (sliding window)
   for (let i = 1; i < input.length; i++) {
+    // Determine whether the sum increases or decreases
     if (input[i - 1] < input[i]) {
       increaseCounter++;
     } else if (input[i - 1] > input[i]) {
@@ -24,19 +25,24 @@ const main = async () => {
     }
   }
 
+  // Print Result to Console
   console.log("Result (Part One):", { increaseCounter, decreaseCounter });
 
   // Re-initialize counters
   increaseCounter = 0;
   decreaseCounter = 0;
 
+  // Loop through input array (size-3 sliding window)
   for (let i = 3; i < input.length; i++) {
+    // Get the first and second 3-size sliding windows
     const firstWindow = [input[i - 3], input[i - 2], input[i - 1]];
     const secondWindow = [input[i - 2], input[i - 1], input[i]];
 
+    // Calculate the sum of the inputs in each each window
     const firstWindowSum = firstWindow.reduce((a, b) => a + b, 0);
     const secondWindowSum = secondWindow.reduce((a, b) => a + b, 0);
 
+    // Determine whether the sum increases or decreases
     if (firstWindowSum < secondWindowSum) {
       increaseCounter++;
     } else if (firstWindowSum > secondWindowSum) {
@@ -44,6 +50,7 @@ const main = async () => {
     }
   }
 
+  // Print Result to Console
   console.log("Result (Part Two):", { increaseCounter, decreaseCounter });
 };
 
